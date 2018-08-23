b59df241cac5642126b067e2c9eb18817ad01691
michalsiwek <siwekmichal21@gmail.com>
2018-08-23 16:12:19 +0200

    Accounts list

---
+++ BlogManager/Models/AccountsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BlogManager.Models.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogManager.Models
+{
+    public class AccountsViewModel
+    {
+        public IEnumerable<Account> Accounts { get; set; }
+    }
+}
