ed6cc887b1440b439e01cb936295da572dd70941
michal.siwek <siwekmichal21@gmail.com>
2018-08-28 22:23:30 +0200

    Custom js bundle

---
+++ BlogManager/Scripts/Custom/preview.js
@@ -0,0 +1,9 @@
+﻿$("#show-preview").click(function () {
+    $("#preview").html("<span>Hello <b>Again</b></span><input id='close-preview' type='button' value='Close' class='btn btn-default btn-bg' />");
+    $("#entry-form").attr("hidden", true);
+})
+
+$("#show-preview").click(function () {
+    $("#preview").html("");
+    $("#entry-form").attr("hidden", true);
+})
