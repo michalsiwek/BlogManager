64c4d1acd42c906c65142f0a111b91aff6e38bb9
Michal Siwek <michal.siwek@ceneo.pl>
2018-10-03 15:54:36 +0200

    Base classes

---
+++ BlogManager/Models/Galleries/Gallery.cs
@@ -0,0 +1,20 @@
+﻿using BlogManager.Models.Accounts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogManager.Models.Galleries
+{
+    public class Gallery
+    {
+        public int Id { get; set; }
+        public Account Account { get; set; }
+        public List<Picture> Pictures { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
