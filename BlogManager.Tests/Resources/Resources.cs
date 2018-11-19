8a91d5a5146ed6ff56d5583338c2cb0cb0c0e6c8
Michal Siwek <michal.siwek@ceneo.pl>
2018-11-19 16:14:54 +0100

    readonly resources

--- BlogManager.Tests/Resources/Resources.cs
+++ BlogManager.Tests/Resources/Resources.cs
@@ -8,19 +8,19 @@ namespace BlogManager.Tests.Resources
 {
     public static class Resources
     {
-        public static string Email = "test@bm.com";
+        public static readonly string Email = "test@bm.com";
 
-        public static string NewSuffix = "new";
-        public static string ModificationSuffix = "mod";
+        public static readonly string NewSuffix = "new";
+        public static readonly string ModificationSuffix = "mod";
 
-        public static string NewEntryTitle = $"Test Title {NewSuffix}";
-        public static string NewEntryDescription = $"Test Description {NewSuffix}";
-        public static string NewEntryContent = $" \r\n\r\n\r\nTest \r\n\r\n\r\n Content \r\n\r\n\r\n {NewSuffix} \r\n\r\n\r\n";
-        public static string NewEntryImgUrl = $"https://www.testimgurl.com/{NewSuffix}";
+        public static readonly string NewEntryTitle = $"Test Title {NewSuffix}";
+        public static readonly string NewEntryDescription = $"Test Description {NewSuffix}";
+        public static readonly string NewEntryContent = $" \r\n\r\n\r\nTest \r\n\r\n\r\n Content \r\n\r\n\r\n {NewSuffix} \r\n\r\n\r\n";
+        public static readonly string NewEntryImgUrl = $"https://www.testimgurl.com/{NewSuffix}";
 
-        public static string ModifiedEntryTitle = $"Test Title {ModificationSuffix}";
-        public static string ModifiedEntryDescription = $"Test Description {ModificationSuffix}";
-        public static string ModifiedEntryContent = $" Test \r\n\r\n\r\n Content \r\n\r\n\r\n {ModificationSuffix} ";
-        public static string ModifiedEntryImgUrl = $"https://www.testimgurl.com/{ModificationSuffix}";
+        public static readonly string ModifiedEntryTitle = $"Test Title {ModificationSuffix}";
+        public static readonly string ModifiedEntryDescription = $"Test Description {ModificationSuffix}";
+        public static readonly string ModifiedEntryContent = $" Test \r\n\r\n\r\n Content \r\n\r\n\r\n {ModificationSuffix} ";
+        public static readonly string ModifiedEntryImgUrl = $"https://www.testimgurl.com/{ModificationSuffix}";
     }
 }
